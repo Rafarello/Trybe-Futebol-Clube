7ff3b8697bacce333fb880dc882409fbb0ef8151
Rafael Moraes <rafael1moraes1rocha@gmail.com>
2022-03-15 14:51:38 -0300

    Commit Inicial - Configurando acesso ao banco de dados local no arquivo das variáveis de ambiente

--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '123456',
+  password: process.env.DB_PASS || '1234',
   database: process.env.DB_NAME || 'TRYBE_FUTEBOL_CLUBE',
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT || '3002',
