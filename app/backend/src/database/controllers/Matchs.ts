63a8c740b8b87c69071b9a5245b19a42990810ae
Rafael Moraes <rafael1moraes1rocha@gmail.com>
2022-03-28 19:32:12 -0300

    Refatorações após a mentoria, encontrado divergências quanto o teste e o retorno de status

--- app/backend/src/database/controllers/Matchs.ts
+++ app/backend/src/database/controllers/Matchs.ts
@@ -8,10 +8,10 @@ class MatchsController {
 
     if (inProgress !== undefined) {
       const queryMatchs = await MatchsServices.getByProgress(inProgress as string);
-      return res.status(200).json(queryMatchs);
+      return res.status(201).json(queryMatchs);
     }
     const allClubs = await MatchsServices.getAll();
-    return res.status(200).json(allClubs);
+    return res.status(201).json(allClubs);
   }
 
   public static async validateMatchInfo(req:Request, res: Response, next: NextFunction) {
@@ -27,14 +27,14 @@ class MatchsController {
   public static async newMatch(req:Request, res: Response) {
     const { body } = req;
     const newMatch = await MatchsServices.newMatch(body);
-    return res.status(200).json(newMatch);
+    return res.status(201).json(newMatch);
   }
 
   public static async updateProgress(req: Request, res: Response) {
     const { id } = req.params;
     await MatchsServices.updateProgress(id);
 
-    return res.status(200).send();
+    return res.status(201).send();
   }
 
   public static async updateMatchResult(req: Request, _res: Response, next: NextFunction) {
